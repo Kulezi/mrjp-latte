dc82c12f18a896277a813e9a621b7589954b2a10
Paweł Putra <pawel.putra@scylladb.com>
2022-11-28 21:12:38 +0100

    prepare frontend submission

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -1,10 +1,7 @@
 package compiler
 
 import (
-	"fmt"
 	"latte/parser"
-	"path"
-	"strings"
 
 	. "latte/compiler/frontend"
 )
@@ -16,24 +13,24 @@ type compiler struct {
 }
 
 func CompileX64(filename string) error {
-	basename := strings.TrimSuffix(filename, path.Ext(filename))
+	// basename := strings.TrimSuffix(filename, path.Ext(filename))
 
-	asm, err := genX64(filename)
+	_, err := genX64(filename)
 	if err != nil {
-		return fmt.Errorf("code generation failed: %w", err)
+		return err
 	}
 
-	if err := saveAssembly(asm, basename+".s"); err != nil {
-		return fmt.Errorf("failed to save assembly to .s file: %w", err)
-	}
+	// if err := saveAssembly(asm, basename+".s"); err != nil {
+	// 	return fmt.Errorf("failed to save assembly to .s file: %w", err)
+	// }
 
-	if err := compileNASM(basename+".s", basename+".o"); err != nil {
-		return fmt.Errorf("nasm compilation error: %w", err)
-	}
+	// if err := compileNASM(basename+".s", basename+".o"); err != nil {
+	// 	return fmt.Errorf("nasm compilation error: %w", err)
+	// }
 
-	if err := link(basename+".o", basename); err != nil {
-		return fmt.Errorf("link error: %w", err)
-	}
+	// if err := link(basename+".o", basename); err != nil {
+	// 	return fmt.Errorf("link error: %w", err)
+	// }
 
 	return nil
 }
